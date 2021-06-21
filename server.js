3d4a39189cc6254310aa979b0ad41a313eb4e2de
ShawenHarker <harkershawen@gmail.com>
2021-06-21 09:37:47 +0200

    created my first RESTful route for my server API.

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ const Listening = () => {
 var server = app.listen(3000, Listening);
 
 app.use(express.static('public'));
+
+const loveDogs = (request, response) => {
+    response.send("I love dogs so much!!!");
+}
+
+app.get("/lovedogs", loveDogs)
