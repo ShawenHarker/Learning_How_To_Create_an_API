2948085d78477c9ab4b63e044a3a8e1347fcaaa4
ShawenHarker <harkershawen@gmail.com>
2021-06-21 10:02:07 +0200

    Created a REST route in my server file and it takes in to parameters in the search bar the client inputs to the browser.

--- server.js
+++ server.js
@@ -12,8 +12,20 @@ var server = app.listen(3000, Listening);
 
 app.use(express.static('public'));
 
+/* 
+The var "data" gets the value of the parameter that the user inputs in the search bar.
+The function below allows me to input two values to my search bar to complete a route to my server. 
+The first value is the name of the thing I want to search and the last is the amount of times I want to loop through it and have it printed out on the screen. 
+*/
 const loveDogs = (request, response) => {
-    response.send("I love dogs so much!!!");
+    let data = request.params;
+    let num = data.num;
+    let reply = "";
+    for (let i = 0; i < num; i++) {
+        reply += `I love ${data.dogs} too!!!`;
+    } 
+    response.send(reply);
 }
 
-app.get("/lovedogs", loveDogs)
+// The colon in front of the dog and num, indicate that it is a parameter and it can be anything the client types. 
+app.get("/search/:dogs/:num", loveDogs)
